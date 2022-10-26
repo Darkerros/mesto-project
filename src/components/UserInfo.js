74629549e9ac81fdfe40512de2b255928ceda570
darker <darkerro@yandex.ru>
2022-10-27 00:06:47 +0300

    Feat: create UserInfo Class and Delete unused files

---
+++ src/components/UserInfo.js
@@ -0,0 +1,52 @@
+
+export default class UserInfo {
+    constructor({nameElementSelector, aboutElementSelector, avatarElementSelector,apiGetUserFn,apiSetUserInfoFn}) {
+        this._nameElement = document.querySelector(nameElementSelector)
+        this._aboutElement = document.querySelector(aboutElementSelector)
+        this._avatarElement = document.querySelector(avatarElementSelector)
+
+        this._getUserFn = apiGetUserFn
+        this._setUserInfoFn = apiSetUserInfoFn
+
+        this.name = ''
+        this.about = ''
+        this.avatar = ''
+        this._id = ''
+        this.cohort = ''
+    }
+
+    getUserInfo() {
+        return this._getUserFn().then(userInfo => {
+            this.name = userInfo.name
+            this.about = userInfo.about
+            this.avatar = userInfo.avatar
+            this._id = userInfo._id
+            this.cohort = userInfo.cohort
+            this._render()
+            return {...userInfo}
+        })
+    }
+
+    setUserInfo(name,about) {
+        return this._setUserInfoFn({name: name,about : about}).then(userInfo => {
+            this.name = userInfo.name
+            this.about = userInfo.about
+            this._render()
+            return {...userInfo}
+        })
+    }
+
+    _render(){
+        this._nameElement.textContent = this.name
+        this._aboutElement.textContent = this.about
+        this._avatarElement.src = this.avatar
+    }
+
+    isIncludeCurrentProfile(idList) {
+        return idList.some(id => this._id === id)
+    }
+
+    isCurrentProfile(id) {
+        return this._id === id
+    }
+}
